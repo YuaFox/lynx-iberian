4eb233d515e5bfbaf9bb3234852d697f3c69ab40
yua <30082001+YuaFox@users.noreply.github.com>
2023-02-04 18:05:25 +0100

    Added support for Mastodon Version 2023.2 ready

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:17
 RUN mkdir /app
-COPY build/libs/lynxiberian-2023.1.jar /app
+COPY build/libs/lynxiberian-2023.2.jar /app
 COPY ./scripts /app
 WORKDIR /app
 CMD ["sh", "app.sh"]
---
+++ src/main/java/dev/yua/lynxiberian/drivers/publisher/MastodonPublisher.java
@@ -0,0 +1,83 @@
+package dev.yua.lynxiberian.drivers.publisher;
+
+import dev.yua.lynxiberian.drivers.PublishDriver;
+import dev.yua.lynxiberian.models.entity.Post;
+import okhttp3.*;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+
+@Component
+public class MastodonPublisher extends PublishDriver {
+
+    private OkHttpClient client;
+
+    private String domain;
+    private String token;
+
+    @Override
+    public String getName() {
+        return "mastodon";
+    }
+
+    @Override
+    public void onLoad() {
+        this.client = new OkHttpClient();
+        this.domain = System.getenv("MASTODON_DOMAIN");
+        this.token = System.getenv("MASTODON_TOKEN");
+    }
+
+    @Override
+    public void publish(Post post) {
+        long mediaId = this.uploadMedia(new File(post.getPath()));
+        this.createStatus(post.getCaption(), mediaId);
+    }
+
+    public void createStatus(String caption, long mediaId){
+        FormBody.Builder requestBuilder = new FormBody.Builder();
+        if(caption != null && !caption.isEmpty()){
+            requestBuilder.add("status", caption);
+        }
+        requestBuilder.add("media_ids[]", String.valueOf(mediaId));
+
+        this.post("/api/v1/statuses", requestBuilder.build()).close();
+    }
+
+
+    public long uploadMedia(File file) {
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", "fox.jpg",
+                        RequestBody.create(MediaType.parse("application/octet-stream"),
+                                        file))
+                .build();
+
+        try (Response response = this.post("/api/v2/media", requestBody)) {
+            return new JSONObject(response.body().string()).getLong("id");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public Response post(String endpoint, RequestBody requestBody){
+        Request request = new Request.Builder()
+                .url("https://"+this.domain+endpoint)
+                .addHeader("Authorization", "Bearer " + this.token)
+                .post(requestBody)
+                .build();
+
+        Call call = client.newCall(request);
+        Response response = null;
+        try {
+            response = call.execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return response;
+    }
+
+
+}
