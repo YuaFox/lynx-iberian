1b15f87393d9aaa46ba751583990db12ae19e261
yua <30082001+YuaFox@users.noreply.github.com>
2023-03-29 21:29:53 +0200

    Clean

--- src/main/java/dev/yua/lynxiberian/repositories/RedditFilterRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.yua.lynxiberian.repositories;
-
-import dev.yua.lynxiberian.models.RedditFilter;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RedditFilterRepository extends CrudRepository<RedditFilter, Long> { }
