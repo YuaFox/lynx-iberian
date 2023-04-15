d2468b1853d50f0b61a02a947e13f26a51f671c0
yua <30082001+YuaFox@users.noreply.github.com>
2023-04-15 10:39:23 +0200

    Updated to use the new Twitter api

--- src/main/java/dev/yua/lynxiberian/repositories/TwitterAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.yua.lynxiberian.repositories;
-
-import dev.yua.lynxiberian.models.TwitterAccount;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface TwitterAccountRepository extends CrudRepository<TwitterAccount, Long> {
-}
